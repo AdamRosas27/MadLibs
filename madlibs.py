d0f7cd852f9e9f3cd3a8f113e8e59706095c6993
Adam Rosas <adamrosas27@gmail.com>
2022-09-24 17:27:39 -0700

    Add comment

--- madlibs.py
+++ madlibs.py
@@ -4,6 +4,7 @@ def choose_story(choice):
     # Match case statement that acts as a switch statement and the output is based on the users input
     match choice:
         case 1:
+            # Prints the story out
             print("You chose the Farm Animal ad-lib:\nThere are many kinds of (adjective) animals that (verb) on a farm. For example, (plural noun; animals) and (plural noun; animals) (verb) eggs, and (noun; food) comes from farm-raised (plural noun; animals). On a dairy farm, (plural noun; animals) and (plural noun; animals) make (noun;beverage) that people drink and also use to make (noun; food) and (noun; food). Some farm animals like (plural noun; animals) and (plural noun; animals) have soft (noun), which is used to make (plural noun; article of clothing) and (plural noun).")
             fa_adj_1, fa_verb_1, fa_pn_1, fa_pn_2, fa_verb_1, fa_n_1, fa_pn_3, fa_pn_4, fa_pn_5, fa_n_2, fa_n_3, fa_n_4, fa_pn_6, fa_pn_7, fa_n_5, fa_pn_8, fa_pn_9 = input(
                 "Enter your desired adjectives, pronouns, nouns, and verbs to complete the story").split()
